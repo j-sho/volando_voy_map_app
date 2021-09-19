87cf3684bf7a5be144ee44db711f98bc954580ef
Julia shorina <juliashorina@Julias-MacBook-Air.local>
2021-09-19 19:02:32 +0200

    about text added

--- src/Components/AboutHolder.jsx
+++ src/Components/AboutHolder.jsx
@@ -8,7 +8,10 @@ export default function AboutHolder(props) {
       <button type="button" className="closeFullInfoWindow" onClick={props.onCloseAbout}></button>
       <div className="about-text">
         <h2>Volando voy</h2>
-        <p>Programa volando voy</p>
+        <p>Volando voy es un programa de televisión de España que se estrenó el 19 de julio de 2015 y es presentado por Jesús Calleja. 
+            La programa tiene 6 temporads y 42 episodios en total. 
+            Volando voy, es un formado de aventuras de Jesús Calleja que junto a ciudadanos españoles recorrerá toda la geografía española desde un helicóptero.
+            La mapa presente todos lo citios de esa programa. Puedes conocer mejor citios espectaculares de España, viendo desde helicoptero los paisajes, y conozer gente de zona.</p>
       </div>
   </div>
   );
