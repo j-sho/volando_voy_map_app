509530d1446f8843e58074cb06f450f646a186d3
Julia shorina <juliashorina@Julias-MacBook-Air.local>
2021-09-21 15:04:45 +0200

    margin to about added

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ const MapContainer = (props) => {
       const items = seriesData.filter(item => item.placeRegion === region);
       setFilteredData(items);
       setCenterRegion(center);
-      setZoom(6.7);
+      setZoom(6.5);
     }
   }
 
